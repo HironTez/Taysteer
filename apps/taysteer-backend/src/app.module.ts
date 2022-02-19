df39244f9a608c75a318c6e465eb08c9babe6d5c
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2022-02-19 16:36:55 +0100

    feat: add logger

--- apps/taysteer-backend/src/app.module.ts
+++ apps/taysteer-backend/src/app.module.ts
@@ -1,15 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReqLogMiddleware } from './middleware/req.log.middleware';
+import { HttpErrorFilter } from './middleware/http-exception.filter';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from './ormconfig';
 import { UserModule } from './resources/users/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(connectionOptions), UserModule, AuthModule],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService, HttpErrorFilter],
 })
 
-export class AppModule { };
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ReqLogMiddleware)
+      .forRoutes({
+        path: '*', method: RequestMethod.ALL
+      });
+  };
+};
--- apps/taysteer-backend/src/ormconfig.ts
+++ apps/taysteer-backend/src/ormconfig.ts
@@ -1,24 +1,20 @@
 import { ConnectionOptions } from 'typeorm';
-import path from 'path';
 import { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DATABASE } from './common/config';
+import { User } from './resources/users/user.model';
 
 export const connectionOptions = {
-    type: 'postgres',
-    host: POSTGRES_HOST,
-    port: POSTGRES_PORT,
-    username: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    database: POSTGRES_DATABASE,
-    migrationsRun: false,
-    synchronize: true,
-    logging: false,
-    keepConnectionAlive: true,
-    autoReconnect: true,
-    reconnectTries: 100,
-    reconnectionInterval: 2000,
-    entities: [path.join(__dirname, './resources/**/*.model.ts')],
-    // migrations: [path.join(__dirname, './migrations/*.ts')],
-    // cli: {
-    //     migrationsDir: './migrations',
-    // },
+  type: 'postgres',
+  host: POSTGRES_HOST,
+  port: POSTGRES_PORT,
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DATABASE,
+  migrationsRun: false,
+  synchronize: true,
+  logging: false,
+  keepConnectionAlive: true,
+  autoReconnect: true,
+  reconnectTries: 100,
+  reconnectionInterval: 2000,
+  entities: [User]
 } as ConnectionOptions;
