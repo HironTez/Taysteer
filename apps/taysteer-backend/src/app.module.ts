e3e9356d13152eb7fe21d3e2dced5827c8e1043e
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2022-02-19 14:05:02 +0100

    feat: connect autorization & users service to the application

--- apps/taysteer-backend/src/app.module.ts
+++ apps/taysteer-backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from './ormconfig';
+import { UserModule } from './resources/users/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [TypeOrmModule.forRoot(connectionOptions), UserModule, AuthModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { };
--- apps/taysteer-backend/src/main.ts
+++ apps/taysteer-backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT_BACKEND } from './common/config';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  console.log(`App is running. Port: ${PORT_BACKEND}`);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      cors: true,
+      logger: ['error', 'warn']
+    }
+  );
   await app.listen(PORT_BACKEND);
+  console.log(`Service is running http://localhost:${PORT_BACKEND}`);
 }
 bootstrap();
