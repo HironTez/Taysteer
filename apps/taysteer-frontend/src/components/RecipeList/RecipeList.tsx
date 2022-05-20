7f39522d2b7c603e319f2d4049170d4f29d39094
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2022-05-20 13:40:19 +0200

    feat: change recipe list width

--- apps/taysteer-frontend/src/components/RecipeList/RecipeList.tsx
+++ apps/taysteer-frontend/src/components/RecipeList/RecipeList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useActions } from '../../hooks/useAction';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
-import './RecipeList.styles.sass';
+import './RecipeList.sass';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import $ from 'jquery';
 
@@ -18,34 +18,36 @@ export const RecipeList: React.FC = () => {
   window.onload = horizontalScrollScript;
 
   return (
-    <div id="recipes-container" className="horizontal-scroll active">
-      <InfiniteScroll
-        dataLength={recipes.length}
-        next={() => {
-          setRecipesPage(page + 1);
-        }}
-        hasMore={loading || (!end && !error)}
-        loader={<h1>Loading...</h1>}
-        endMessage={error ? <h1>Error!</h1> : <h1>The end</h1>}
-        scrollableTarget="recipes-container"
-      >
-        {recipes.map((recipe) => (
-          <div key={recipe.id} className="recipe-min">
-            <img className="image" src={recipe.image} alt="avatar" />
-            <div className="title">{recipe.title}</div>
-            <div className={`rating rating-${recipe.rating}`}>
-              <span className="rating-star 1"></span>
-              <span className="rating-star 2"></span>
-              <span className="rating-star 3"></span>
-              <span className="rating-star 4"></span>
-              <span className="rating-star 5"></span>
+    <section className="recipes">
+      <div id="recipes-container" className="horizontal-scroll active">
+        <InfiniteScroll
+          dataLength={recipes.length}
+          next={() => {
+            setRecipesPage(page + 1);
+          }}
+          hasMore={loading || (!end && !error)}
+          loader={<h1>Loading...</h1>}
+          endMessage={error ? <h1>Error!</h1> : <h1>The end</h1>}
+          scrollableTarget="recipes-container"
+        >
+          {recipes.map((recipe) => (
+            <div key={recipe.id} className="recipe-min">
+              <img className="image" src={recipe.image} alt="avatar" />
+              <div className="title">{recipe.title}</div>
+              <div className={`rating rating-${recipe.rating}`}>
+                <span className="rating-star 1"></span>
+                <span className="rating-star 2"></span>
+                <span className="rating-star 3"></span>
+                <span className="rating-star 4"></span>
+                <span className="rating-star 5"></span>
+              </div>
+              <div className="description">{recipe.description}</div>
+              <div className="readMore">Read more</div>
             </div>
-            <div className="description">{recipe.description}</div>
-            <div className="readMore">Read more</div>
-          </div>
-        ))}
-      </InfiniteScroll>
-    </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+    </section>
   );
 };
 
