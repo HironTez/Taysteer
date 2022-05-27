8fd0c7bac45af34aaed71da5176e663bdf542ff2
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2022-05-27 19:55:08 +0200

    fix: sign in buttons compatibility

--- apps/taysteer-frontend/src/components/account/Account.tsx
+++ apps/taysteer-frontend/src/components/account/Account.tsx
@@ -1,14 +1,13 @@
 import './Account.sass';
 
 export const Account: React.FC = () => {
-
   return (
     <div className="account">
       <a className="sign-up" href="/register">
         Sign up
       </a>
       <a className="sign-in" href="/login">
-        Sign in
+        Log in
       </a>
     </div>
   );
