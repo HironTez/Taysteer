04d2769330080078e37c4a7917566b7381ffc264
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2022-06-08 19:48:42 +0200

    refactor: move recipe to components

---
+++ apps/taysteer-frontend/src/components/recipe/Recipe.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Error } from '../error.animation/Error.animation';
+import { Loading } from '../loading.spinner/Loading.spinner';
+import { useActions } from '../../hooks/useAction';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import './Recipe.sass';
+
+export const Recipe: React.FC = () => {
+  const { id } = useParams();
+
+  const { recipe, loading, error } = useTypedSelector((state) => state.recipe);
+
+  const { fetchRecipe } = useActions();
+
+  useEffect(() => {
+    if (!recipe && !loading && !error) fetchRecipe(String(id));
+  });
+
+  if (recipe && !loading && !error) {
+    return <div className='recipe-container'></div>;
+  } else if (loading) {
+    return <div className='recipe-container'><Loading/></div>;
+  } else {
+    return <div className='recipe-container'><Error/></div>;
+  }
+};
--- apps/taysteer-frontend/src/pages/Recipe/Recipe.tsx
+++ apps/taysteer-frontend/src/pages/Recipe/Recipe.tsx
@@ -1,27 +1,10 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Error } from '../../components/error.animation/Error.animation';
-import { Loading } from '../../components/loading.spinner/Loading.spinner';
-import { useActions } from '../../hooks/useAction';
-import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { Recipe } from '../../components/recipe/Recipe';
 import './Recipe.sass';
 
 export const RecipePage: React.FC = () => {
-  const { id } = useParams();
-
-  const { recipe, loading, error } = useTypedSelector((state) => state.recipe);
-
-  const { fetchRecipe } = useActions();
-
-  useEffect(() => {
-    if (!recipe && !loading && !error) fetchRecipe(String(id) + '1');
-  });
-
-  if (recipe && !loading && !error) {
-    return <div className='recipe-container'></div>;
-  } else if (loading) {
-    return <div className='recipe-container'><Loading/></div>;
-  } else {
-    return <div className='recipe-container'><Error/></div>;
-  }
+  return (
+    <div>
+      <Recipe/>
+    </div>
+  )
 };
