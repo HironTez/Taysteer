8a3a9ec7319f5c20778c5110a3f72aa644e1f09f
HironTez <vitalikos137@gmail.com>
2023-02-28 20:38:03 +0100

    fix: configs

--- apps/taysteer-backend/src/ormconfig.ts
+++ apps/taysteer-backend/src/ormconfig.ts
@@ -1,10 +1,6 @@
 import { ConnectionOptions } from 'typeorm';
 import {
-  POSTGRES_HOST,
-  POSTGRES_PORT,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-  POSTGRES_DATABASE,
+  POSTGRES_URL,
 } from '../../../configs/common/config';
 import { Recipe } from './resources/recipes/recipe.model';
 import { RecipeRating } from './resources/recipes/recipe.rating.model';
@@ -13,11 +9,7 @@ import { Comment } from './resources/recipes/recipe.comment.model';
 
 export const connectionOptions = {
   type: 'postgres',
-  host: POSTGRES_HOST,
-  port: POSTGRES_PORT,
-  username: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-  database: POSTGRES_DATABASE,
+  url: POSTGRES_URL,
   migrationsRun: false,
   synchronize: true,
   logging: false,
