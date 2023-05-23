82ddcebdc7ed49a26434e8e513499fc9b1b18923
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2023-05-23 15:54:39 +0200

    remove main backend resourse

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
 import { env } from 'config';
 
 async function bootstrap() {
@@ -14,7 +15,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, documentConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(env.PORT_BACKEND).then(() => {
     console.log(`Listening on http://localhost:${env.PORT_BACKEND}`);
