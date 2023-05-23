a5ebccccd415a6a3e9ec2ff1b51c1d6edaad4f02
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2023-05-23 12:03:29 +0200

    setup: config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,22 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import config from 'config';
+console.log("🚀 ~ file: main.ts:5 ~ config:", config)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const documentConfig = new DocumentBuilder()
     .setTitle('API')
     .setDescription('The API description')
     .setVersion('1.0')
     .addTag('api')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, documentConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000).then(() => {
-    console.log('Listening on http://localhost:3000');
+  await app.listen(config.PORT_BACKEND).then(() => {
+    console.log(`Listening on http://localhost:${config.PORT_BACKEND}`);
   });
 }
 bootstrap();
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,25 +1,30 @@
-// vite.config.js
 import { defineConfig } from "vitest/config"
+import { loadEnv } from "vite"
 import sucrase from "@rollup/plugin-sucrase"
 
-export default defineConfig({
-  plugins: [
-    sucrase({
-      include: [".js", ".ts", ".jsx", ".tsx"],
-      transforms: ["typescript", "jsx"], // Adjust the transforms as per your project's needs
-    }),
-  ],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    plugins: [
+      sucrase({
+        include: [".js", ".ts", ".jsx", ".tsx"],
+        transforms: ["typescript", "jsx"], // Adjust the transforms as per your project's needs
+      }),
+    ],
+    server: {
+      open: true,
+      port: Number(env.PORT) || undefined,
+    },
+    build: {
+      outDir: "dist",
+      sourcemap: true,
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/setupTests",
+      mockReset: true,
+    },
+  }
 })
