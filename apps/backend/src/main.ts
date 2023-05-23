d7cdeff96e5f59f52f29aa0b2b4a9cf6b394c8ca
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2023-05-23 14:11:41 +0200

    config: remove unused packages

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import config from 'config';
-console.log("🚀 ~ file: main.ts:5 ~ config:", config)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
