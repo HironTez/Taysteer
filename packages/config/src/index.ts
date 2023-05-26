edef726ed977196821542440c610f870e92a6431
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2023-05-26 09:35:44 +0200

    init v3.0

--- packages/config/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { config as configureDotENV } from 'dotenv';
-
-configureDotENV();
-
-export const PORT_BACKEND = Number(process.env.PORT_BACKEND ?? 4000);
-// export const PORT_FRONTEND = Number(process.env.PORT_FRONTEND ?? 3000)
-export const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY;
-export const SESSION_SECRET_SALT = process.env.SESSION_SECRET_SALT;
