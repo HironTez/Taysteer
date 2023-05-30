49d5494f45e9593b4a3cf3839a32261548734dfd
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2023-05-30 11:19:26 +0200

    feat: get user list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,5 @@ import styles from "./page.module.css";
 export default async function Home() {
   const users = await prisma.user.findMany();
 
-  return (
-    <main className={styles.main}>
-      {JSON.stringify(users)}
-    </main>
-  );
+  return <main className={styles.main}>{JSON.stringify(users)}</main>;
 }
