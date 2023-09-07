80456f40fecabbed24ed38cf3357f82aab56517a
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2023-09-07 20:09:57 +0200

    fix: configs

--- apps/taysteer-backend/src/ormconfig.ts
+++ apps/taysteer-backend/src/ormconfig.ts
@@ -1,6 +1,10 @@
 import { ConnectionOptions } from 'typeorm';
 import {
-  POSTGRES_URL,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DATABASE,
 } from '../../../configs/common/config';
 import { Recipe } from './resources/recipes/recipe.model';
 import { RecipeRating } from './resources/recipes/recipe.rating.model';
@@ -9,7 +13,11 @@ import { Comment } from './resources/recipes/recipe.comment.model';
 
 export const connectionOptions = {
   type: 'postgres',
-  url: POSTGRES_URL,
+  host: POSTGRES_HOST,
+  port: POSTGRES_PORT,
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DATABASE,
   migrationsRun: false,
   synchronize: true,
   logging: false,
@@ -17,8 +25,6 @@ export const connectionOptions = {
   autoReconnect: true,
   reconnectTries: 100,
   reconnectionInterval: 2000,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: false,
   entities: [User, Recipe, RecipeRating, Comment],
 } as ConnectionOptions;
