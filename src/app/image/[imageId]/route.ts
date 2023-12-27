1998f3b605811a53c67bd2514f70f34224ce5c38
Hiron Tez <81362525+HironTez@users.noreply.github.com>
2023-12-27 19:26:20 +0100

    feat: image api content headers

--- src/app/image/[imageId]/route.ts
+++ src/app/image/[imageId]/route.ts
@@ -1,15 +1,26 @@
 import { prisma } from "@/db";
 import { StatusCodes } from "http-status-codes";
+import { NextResponse, type NextRequest } from "next/server";
 
 export const GET = async (
-  _request: Request,
+  request: NextRequest,
   { params: { imageId } }: { params: { imageId: string } },
 ) => {
   try {
     const image = await prisma.image.findUnique({ where: { id: imageId } });
-    if (image?.value) return new Response(image.value);
-  } catch {
-    return new Response(null, { status: StatusCodes.BAD_REQUEST });
+
+    if (!image?.value) {
+      return new NextResponse(null, { status: StatusCodes.NOT_FOUND });
+    }
+
+    const headers = new Headers(request.headers);
+
+    headers.set("Content-Type", image.mime);
+    headers.set("Content-Length", image.value.length.toString());
+
+    return new NextResponse(image.value, { headers });
+  } catch (e) {
+    console.error("🚀 ~ file: route.ts:25 ~ e:", e);
+    return new NextResponse(null, { status: StatusCodes.BAD_REQUEST });
   }
-  return new Response(null, { status: StatusCodes.NOT_FOUND });
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-url", request.url);
-  requestHeaders.set("x-pathname", request.nextUrl.pathname);
-  requestHeaders.set("x-search", request.nextUrl.search);
+  const headers = new Headers(request.headers);
+  headers.set("x-url", request.url);
+  headers.set("x-pathname", request.nextUrl.pathname);
+  headers.set("x-search", request.nextUrl.search);
 
   return NextResponse.next({
     request: {
-      headers: requestHeaders,
+      headers,
     },
   });
 }
